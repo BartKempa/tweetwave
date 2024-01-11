378e2c1749cb476e0a4c6e6bc371545796fd0778
BartKempa <bartekkempa@gmail.com>
2024-01-11 14:59:43 +0100

    pola, konstrutor i gettery dla klasy dto TweetDto

--- src/main/java/com/example/tweetwave/domain/api/TweetDto.java
+++ src/main/java/com/example/tweetwave/domain/api/TweetDto.java
@@ -1,4 +1,27 @@
 package com.example.tweetwave.domain.api;
 
+import java.time.LocalDateTime;
+
 public class TweetDto {
+    private String description;
+    private LocalDateTime dateAdded;
+    private String url;
+
+    public TweetDto(String description, LocalDateTime dateAdded, String url) {
+        this.description = description;
+        this.dateAdded = dateAdded;
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
--- src/main/java/com/example/tweetwave/domain/api/TweetService.java
+++ src/main/java/com/example/tweetwave/domain/api/TweetService.java
@@ -1,4 +1,5 @@
 package com.example.tweetwave.domain.api;
 
 public class TweetService {
+
 }
