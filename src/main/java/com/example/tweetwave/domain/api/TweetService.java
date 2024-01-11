3fd81d46976d5e861a07117302b728369e7c9886
BartKempa <bartekkempa@gmail.com>
2024-01-11 15:17:14 +0100

    mapowanie Tweet do TweetDto

--- src/main/java/com/example/tweetwave/domain/api/TweetService.java
+++ src/main/java/com/example/tweetwave/domain/api/TweetService.java
@@ -1,5 +1,25 @@
 package com.example.tweetwave.domain.api;
 
-public class TweetService {
+import com.example.tweetwave.domain.tweet.Tweet;
+import com.example.tweetwave.domain.tweet.TweetDao;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TweetService {
+    private final TweetDao tweetDao = new TweetDao();
+
+    public List<TweetDto> findAll(){
+        return tweetDao.findAll()
+                .stream()
+                .map(TweetMapper::map)
+                .collect(Collectors.toList());
+    }
+
+
+    private static class TweetMapper {
+        static TweetDto map(Tweet tweet) {
+            return new TweetDto(tweet.getDescription(), tweet.getDateAdded(), tweet.getUrl());
+        }
+    }
 }
