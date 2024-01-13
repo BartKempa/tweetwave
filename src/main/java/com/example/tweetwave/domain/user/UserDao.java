062b26821eaf5e423961bc42b5ac58d61e57c662
BartKempa <bartekkempa@gmail.com>
2024-01-13 09:50:36 +0100

    metoda findById w klasie User

--- src/main/java/com/example/tweetwave/domain/user/UserDao.java
+++ src/main/java/com/example/tweetwave/domain/user/UserDao.java
@@ -1,4 +1,46 @@
 package com.example.tweetwave.domain.user;
 
+import com.example.tweetwave.configuration.DataSourceProvider;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class UserDao {
+    private DataSource dataSource;
+
+    public UserDao() {
+        try {
+            dataSource = DataSourceProvider.getDataSource();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<User> findById(int id){
+        final String query = """
+                SELECT
+                    id, userName, email, password, registrationDate
+                FROM
+                    user
+                WHERE
+                    id = ?
+                """;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next())
+                return Optional.of(mapRow(resultSet));
+            else
+                return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
