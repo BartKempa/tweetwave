3854af998ce88b1da2b5dc81a760cd4b7a6d6bab
BartKempa <bartekkempa@gmail.com>
2024-01-19 09:09:01 +0100

    metoda do rejestracji użytkownaika w UserService

--- src/main/java/com/example/tweetwave/domain/api/UserService.java
+++ src/main/java/com/example/tweetwave/domain/api/UserService.java
@@ -1,4 +1,17 @@
 package com.example.tweetwave.domain.api;
 
+import com.example.tweetwave.domain.user.User;
+import com.example.tweetwave.domain.user.UserDao;
+
+import java.time.LocalDateTime;
+
 public class UserService {
+    private UserDao userDao = new UserDao();
+
+    public void userRegistration(UserRegistrationDto userRegistrationDto){
+        User user = UserMapper.map(userRegistrationDto);
+        userDao.saveUser(user);
+    }
+
+
 }
