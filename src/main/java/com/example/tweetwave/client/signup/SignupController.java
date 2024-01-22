1808a933fc2d78c242c4f159c7732b973e4e9c8e
BartKempa <bartekkempa@gmail.com>
2024-01-22 09:10:37 +0100

    zapisaywania użytkownika do bazy

--- src/main/java/com/example/tweetwave/client/signup/SignupController.java
+++ src/main/java/com/example/tweetwave/client/signup/SignupController.java
@@ -1,4 +1,31 @@
 package com.example.tweetwave.client.signup;
 
-public class SignupController {
+import com.example.tweetwave.domain.api.UserRegistrationDto;
+import com.example.tweetwave.domain.api.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/signup")
+public class SignupController extends HttpServlet {
+    private final UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/signup.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserRegistrationDto userRegistrationDto = getUserInfo(req);
+        userService.userRegistration(userRegistrationDto);
+        resp.sendRedirect(req.getContextPath());
+
+    }
+
+
 }
