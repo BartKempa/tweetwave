1eca31508cf99f636c14e2a44edfab19c0a3ca4d
BartKempa <bartekkempa@gmail.com>
2024-01-26 12:56:47 +0100

    wyświetlanie foto w jsp

--- src/main/java/com/example/tweetwave/client/photo/PhotoController.java
+++ src/main/java/com/example/tweetwave/client/photo/PhotoController.java
@@ -11,5 +11,18 @@ import java.io.IOException;
 
 @WebServlet("/photo/*")
 public class PhotoController extends HttpServlet {
+    private TweetService tweetService = new TweetService();
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String photoId = req.getPathInfo().substring(1);
+        byte[] photoData = tweetService.getPhotoById(photoId);
+        if (photoData != null) {
+            resp.setContentType("image/jpeg");
+            resp.setContentLength(photoData.length);
+            resp.getOutputStream().write(photoData);
+        } else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 }
