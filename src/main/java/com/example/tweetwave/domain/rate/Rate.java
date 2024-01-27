744be043556447c555eb6a4b22a7fd51055ff5f8
BartKempa <bartekkempa@gmail.com>
2024-01-27 11:19:08 +0100

    pola, gettery, enuym i konstruktor klasy Rate

--- src/main/java/com/example/tweetwave/domain/rate/Rate.java
+++ src/main/java/com/example/tweetwave/domain/rate/Rate.java
@@ -1,6 +1,37 @@
 package com.example.tweetwave.domain.rate;
 
+import java.time.LocalDateTime;
+
 public class Rate {
+    private Integer userId;
+    private Integer tweetId;
+    private Type type;
+    private LocalDateTime dateAdded;
 
+    public Rate(Integer userId, Integer tweetId, Type type, LocalDateTime dateAdded) {
+        this.userId = userId;
+        this.tweetId = tweetId;
+        this.type = type;
+        this.dateAdded = dateAdded;
+    }
 
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getTweetId() {
+        return tweetId;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public enum Type{
+        LIKE, DISLIKE
+    }
 }
