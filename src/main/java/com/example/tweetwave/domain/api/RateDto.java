9b73de3f9bdb28b6943ca9e11378d9f9b74c318c
BartKempa <bartekkempa@gmail.com>
2024-01-27 11:58:25 +0100

    pola, kontruktor i gettery klasy RateDto

--- src/main/java/com/example/tweetwave/domain/api/RateDto.java
+++ src/main/java/com/example/tweetwave/domain/api/RateDto.java
@@ -1,4 +1,25 @@
 package com.example.tweetwave.domain.api;
 
 public class RateDto {
+    private String userName;
+    private Integer tweetId;
+    private String type;
+
+    public RateDto(String userName, Integer tweetId, String type) {
+        this.userName = userName;
+        this.tweetId = tweetId;
+        this.type = type;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Integer getTweetId() {
+        return tweetId;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
