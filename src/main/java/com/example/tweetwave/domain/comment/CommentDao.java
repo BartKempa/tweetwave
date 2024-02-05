e47ee52c402460138760ef4b04c84ef575a11256
BartKempa <bartekkempa@gmail.com>
2024-02-05 09:32:21 +0100

    metoda do zapisaywania komentarza w klasa CommentDao

--- src/main/java/com/example/tweetwave/domain/comment/CommentDao.java
+++ src/main/java/com/example/tweetwave/domain/comment/CommentDao.java
@@ -1,5 +1,41 @@
 package com.example.tweetwave.domain.comment;
 
+import com.example.tweetwave.configuration.DataSourceProvider;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
+
 public class CommentDao {
-    
+    private DataSource dataSource;
+
+    public CommentDao() {
+        try {
+            dataSource = DataSourceProvider.getDataSource();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void save(Comment comment){
+        final String query = """
+                INSERT INTO 
+                    comment (user_id, tweet_id, date_added, description)
+                VALUES 
+                    (?, ?, ?, ?)
+                """;
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+           statement.setInt(1, comment.getUserId());
+           statement.setInt(1, comment.getTweetId());
+           statement.setObject(3, comment.getDateAdded());
+           statement.setString(4, comment.getDescription());
+           statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next())
+                comment.setId(generatedKeys.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
