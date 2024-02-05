c321d68514ce4881882ffec40068ec65a9638d56
BartKempa <bartekkempa@gmail.com>
2024-02-05 09:20:23 +0100

    klasa Comment pola, konstruktor i gettery

--- src/main/java/com/example/tweetwave/domain/comment/Comment.java
+++ src/main/java/com/example/tweetwave/domain/comment/Comment.java
@@ -1,4 +1,42 @@
 package com.example.tweetwave.domain.comment;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Integer id;
+    private Integer userId;
+    private Integer tweetId;
+    private LocalDateTime dateAdded;
+    private String description;
+
+    public Comment(Integer userId, Integer tweetId, LocalDateTime dateAdded, String description) {
+        this.userId = userId;
+        this.tweetId = tweetId;
+        this.dateAdded = dateAdded;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getTweetId() {
+        return tweetId;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
