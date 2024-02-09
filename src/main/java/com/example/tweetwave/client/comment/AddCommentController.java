2d7311413a659e6d7616d75ac2efc3c61b1fdc35
BartKempa <bartekkempa@gmail.com>
2024-02-09 08:50:03 +0100

    doPost w AddCommentController

--- src/main/java/com/example/tweetwave/client/comment/AddCommentController.java
+++ src/main/java/com/example/tweetwave/client/comment/AddCommentController.java
@@ -1,9 +1,16 @@
 package com.example.tweetwave.client.comment;
 
+import com.example.tweetwave.domain.api.CommentDto;
+import com.example.tweetwave.domain.api.CommentService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.HttpMethodConstraint;
 import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet
 @ServletSecurity(
@@ -12,5 +19,14 @@ import jakarta.servlet.http.HttpServlet;
         }
 )
 public class AddCommentController extends HttpServlet {
-    
+        CommentService commentService = new CommentService();
+
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                CommentDto commentDto = createCommentDto(req);
+                commentService.add(commentDto);
+                resp.sendRedirect(req.getContextPath() + "/");
+        }
+
+
 }
